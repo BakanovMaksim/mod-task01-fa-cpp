9b371402d91d2a8557ee9a8b77ca2eb04b1ee0ac
maksim <bakanovmp12@gmail.com>
2021-03-22 10:53:57 +0300

    mod-task01-fa-cpp: adding a solution to a problem

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,12 +1,111 @@
+#include <cmath.h>
+#include <ctype.h>
+#include "fun.h"
+
 unsigned int faStr1(const char *str)
 {
-    return 0;
+    int index = 0;
+    int count = 0;
+    bool inWord = false;
+    bool isNumber = false;
+
+    while(str[index] != '\0')
+    {
+        if (str[index] != ' ' && !inWord)
+        {
+            inWord = true;
+
+            if (isDigit(str[index]))
+            {
+                isNumber = true;
+            }
+        }
+        else if ((str[index] != ' ') && inWord && isDigit(str[index]))
+        {
+            isNumber = true;
+        }
+        else if(str[index] == ' ')
+        {
+            if(!isNumber && inWord)
+            {
+                count++;
+            }
+
+            inWord = false;
+            isNumber = false;
+        }
+
+        index++;
+    }
+
+    return count;
 }
 unsigned int faStr2(const char *str)
 {
-    return 0;
+    int index = 0;
+    int count = 0;
+    bool inWord = false;
+    isUpperSymbol = false;
+    isLowerSymbol = true;
+
+    while(str[index] != '\0')
+    {
+        if((str[index] != ' ') && !inWord)
+        {
+            inWord = true;
+
+            if(('A' <= str[index]) && (str[index] <= 'Z'))
+            {
+                isUpperSymbol = true;
+            }
+        }
+        else if((str[index] != ' ') && inWord && (('a' > str[index]) || (str[index] > 'z')))
+        {
+            isLowerSymbol = false;
+        }
+        else if(str[index] == ' ')
+        {
+            if(inWord && isUpperSymbol && isLowerSymbol)
+            {
+                count++;
+            }
+
+            inWord = false;
+            isUpperSymbol = false;
+            isLowerSymbol = true;
+        }
+
+        index++;
+    }
+
+    return count;
 }
 unsigned int faStr3(const char *str)
 {
-    return 0;
+    int index = 0;
+    int count = 0;
+    int countSymbolsWords = 0;
+    bool inWord = false;
+
+    while(str[index] != '\0')
+    {
+        if ((str[index] != ' ') && !inWord)
+        {
+            inWord = true;
+            countSymbolsWords++;
+            count++;
+        }
+        else if ((str[index] != ' ') && inWord)
+        {
+            countSymbolsWords++;
+        }
+        else if ((str[index] == ' ') && inWord)
+        {
+            inWord = false;
+        }
+
+        index++;
+    }
+
+    return round(countSymbolsWords/count);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,10 @@
 
 int main()
 {
+    faStr1(" 3534g gsdgsd4  wdfsd 34f ");
+    faStr2("Jgdsg Rdsg Ld tretb");
+    faStr3("fsd sdf as asfa jaij  ieamfF");
+
+    system("pause");
     return 0;
 }
